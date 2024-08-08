1ae3ff0bb539b7472755544cee0885507c6d0841
lokesh pal <118580452+Lokesh200129@users.noreply.github.com>
2024-08-08 03:48:43 +0000

    pre-final

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,14 +1,52 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import WeatherComp from './component/WeatherComp'
 // import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(true)
+  // console.log(data)
+  const [place, setPlace] = useState('noida')
+  const [Search, setSearch] =useState(false)
 
+  useEffect(() => {
+    if(!Search) return
+    async function fetchData() {
+      try {
+        const res = await fetch(`https://api.weatherapi.com/v1/current.json?key=05e520217cce4953a6b32218240708&q=${place}`)
+        const data = await res.json();
+        setData(() => data);
+        setLoading((pre) => pre != pre)
+      }
+
+      catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+      }
+      setSearch(false)
+    }
+    fetchData()
+  }, [place, Search])
+
+
+  // Conditional rendering
+  
+
+
+  const handler = (e) => {
+    setPlace(e.target.value)
+  }
+  const getResult = () => {
+    setSearch(true)
+  }
   return (
     <>
+      <div className="input">
+        <input type="text" placeholder='Enter your city' value={place} onChange={handler} />
+        <button onClick={getResult} > Search </button>
+      </div>
+    {loading ?  <p> Loading... </p> : <WeatherComp data={data} />}
+
       
-      <WeatherComp />
     </>
   )
 }
--- weather-app/src/component/WeatherComp.jsx
+++ weather-app/src/component/WeatherComp.jsx
@@ -1,23 +1,29 @@
-
-function WeatherComp (){
+import React from 'react';
+function WeatherComp ({data}){
+   
+     // Conditional rendering
+    if(!data) return <p> Fetching Data...</p>;
     return (
         <>
-            <div className="bg-neutral-100 text-black h-30  mt-4  w-1/2 h-full m-auto p-4" >
-                <div  className="flex flex-row space-x-32 ">
-                    <div className="date">day/date</div>
-                    <div className="temperature">temperature</div>
+            <div className="bg-neutral-100 text-black h-30  mt-4  w-3/4 h-full m-auto p-4" >
+                <div  className="flex flex-row space-x-8 ">
+                    <div>Place: <strong>{data.location.name}</strong></div>
+                    <div className="date">day/date {data.location.localtime} </div>
+                    <div className="temperature">Temp {data.current.temp_c}</div>
                     <button className="detailBtn"> Detail</button>
                     <br/>
                 </div>
-                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae officiis id tenetur est,</div>
+               
+                <div> <strong>Weather Condition : </strong> {data.current.condition.text} </div>
+               
                 <div className="flex flex-row space-x-28">                
                     <div className="flex flex-row space-x-28"> 
-                        <div>Real feel </div>
-                        <div>32</div>
+                        <div>Real feel  </div>
+                        <div>{data.current.feelslike_c}</div>
                     </div>
                     <div className="flex flex-row space-x-28"> 
-                        <div>Max UV Index </div>
-                        <div>32</div>
+                        <div>Humadity</div>
+                        <div>{data.current.humidity}</div>
                     </div>
                 </div> 
                 <div className="flex flex-row space-x-28" >                
@@ -27,7 +33,7 @@ function WeatherComp (){
                     </div>
                     <div className="flex flex-row space-x-28"> 
                         <div>Wind</div>
-                        <div>32</div>
+                        <div>{data.current.wind_kph} Km/h</div>
                     </div>
                 </div> 
 
