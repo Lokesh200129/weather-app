77729fa0f2d8a9cabdcac26b7d1455249b943893
lokesh pal <118580452+Lokesh200129@users.noreply.github.com>
2024-08-12 14:07:53 +0000

    update-3

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -3,47 +3,10 @@ import WeatherComp from './component/WeatherComp'
 // import './App.css'
 
 function App() {
-  const [data, setData] = useState(null)
-  const [loading, setLoading] = useState(true)
-  // console.log(data)
-  const [place, setPlace] = useState('noida')
-  const [Search, setSearch] =useState(false)
-
-  useEffect(() => {
-    if(!Search) return
-    async function fetchData() {
-      try {
-        const res = await fetch(`https://api.weatherapi.com/v1/current.json?key=05e520217cce4953a6b32218240708&q=${place}`)
-        const data = await res.json();
-        setData(() => data);
-        setLoading((pre) => pre != pre)
-      }
-
-      catch (error) {
-        console.error('There was a problem with the fetch operation:', error);
-      }
-      setSearch(false)
-    }
-    fetchData()
-  }, [place, Search])
-
-
-  // Conditional rendering
   
-
-
-  const handler = (e) => {
-    setPlace(e.target.value)
-  }
-  const getResult = () => {
-    setSearch(true)
-  }
+  
   return (
-    <>
-    <WeatherComp data={data} />
-
       <WeatherComp/>
-    </>
   )
 }
 
